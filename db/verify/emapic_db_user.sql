6c8b9efe84d230136056ad98e4cedd70caba4b81
Jorge López Fernández <jlopez@emapic.es>
2017-09-11 18:52:02 +0200

    Fixed a serious mistake in sqitch verify files

--- db/verify/emapic_db_user.sql
+++ db/verify/emapic_db_user.sql
@@ -2,21 +2,21 @@
 
 BEGIN;
 
-SELECT has_schema_privilege(:'emapic_db_user', 'base_layers', 'usage');
-SELECT has_schema_privilege(:'emapic_db_user', 'public', 'usage');
-SELECT has_schema_privilege(:'emapic_db_user', 'metadata', 'usage');
-SELECT has_schema_privilege(:'emapic_db_user', 'opinions', 'create, usage');
-SELECT has_table_privilege(:'emapic_db_user', 'base_layers.municipalities', 'select');
-SELECT has_table_privilege(:'emapic_db_user', 'base_layers.provinces', 'select');
-SELECT has_table_privilege(:'emapic_db_user', 'base_layers.countries', 'select');
-SELECT has_table_privilege(:'emapic_db_user', 'users', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'roles', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'rel_users_roles', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'metadata.surveys', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'metadata.questions', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'metadata.answers', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'metadata.emapic_opinions', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'metadata.geolocation_distances', 'select, insert, delete, update');
-SELECT has_table_privilege(:'emapic_db_user', 'metadata.votes', 'select, insert, delete, update');
+SELECT 1/CASE WHEN has_schema_privilege(:'emapic_db_user', 'base_layers', 'usage') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_schema_privilege(:'emapic_db_user', 'public', 'usage') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_schema_privilege(:'emapic_db_user', 'metadata', 'usage') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_schema_privilege(:'emapic_db_user', 'opinions', 'create, usage') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'base_layers.municipalities', 'select') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'base_layers.provinces', 'select') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'base_layers.countries', 'select') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'users', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'roles', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'rel_users_roles', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'metadata.surveys', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'metadata.questions', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'metadata.answers', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'metadata.emapic_opinions', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'metadata.geolocation_distances', 'select, insert, delete, update') THEN 1 ELSE 0 END;
+SELECT 1/CASE WHEN has_table_privilege(:'emapic_db_user', 'metadata.votes', 'select, insert, delete, update') THEN 1 ELSE 0 END;
 
 ROLLBACK;
