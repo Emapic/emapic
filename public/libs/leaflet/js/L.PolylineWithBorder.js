b587b0b2f610e0f11aa0cceff6166935421b5cf8
Jorge López Fernández <lopez.fernandez.jorge@gmail.com>
2018-12-11 16:20:13 +0100

    Add multigeometry support to PolylineWithBorder

--- public/libs/leaflet/js/L.PolylineWithBorder.js
+++ public/libs/leaflet/js/L.PolylineWithBorder.js
@@ -57,59 +57,6 @@ L.PolylineWithBorder = L.Polyline.extend({
 });
 
 L.GeoJSONWithBorder = L.GeoJSON.extend({
-    geometryToLayerWithBorder: function(geojson, options) {
-        var geometry = geojson.type === 'Feature' ? geojson.geometry : geojson,
-            coords = geometry ? geometry.coordinates : null,
-            layers = [],
-            pointToLayer = options && options.pointToLayer,
-            _coordsToLatLng = options && options.coordsToLatLng || L.GeoJSON.coordsToLatLng,
-            latlng, latlngs, i, len;
-
-        if (!coords && !geometry) {
-            return null;
-        }
-
-        switch (geometry.type) {
-            case 'Point':
-                latlng = _coordsToLatLng(coords);
-                return pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng);
-
-            case 'MultiPoint':
-                for (i = 0, len = coords.length; i < len; i++) {
-                    latlng = _coordsToLatLng(coords[i]);
-                    layers.push(pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng));
-                }
-                return new L.FeatureGroup(layers);
-
-            case 'LineString':
-            case 'MultiLineString':
-                latlngs = L.GeoJSON.coordsToLatLngs(coords, geometry.type === 'LineString' ? 0 : 1, _coordsToLatLng);
-                return new L.PolylineWithBorder(latlngs, options);
-
-            case 'Polygon':
-            case 'MultiPolygon':
-                latlngs = L.GeoJSON.coordsToLatLngs(coords, geometry.type === 'Polygon' ? 1 : 2, _coordsToLatLng);
-                return new L.Polygon(latlngs, options);
-
-            case 'GeometryCollection':
-                for (i = 0, len = geometry.geometries.length; i < len; i++) {
-                    var layer = L.GeoJSON.geometryToLayer({
-                        geometry: geometry.geometries[i],
-                        type: 'Feature',
-                        properties: geojson.properties
-                    }, options);
-
-                    if (layer) {
-                        layers.push(layer);
-                    }
-                }
-                return new L.FeatureGroup(layers);
-
-            default:
-                throw new Error('Invalid GeoJSON object.');
-        }
-    },
-
     addData: function (geojson) {
         var features = L.Util.isArray(geojson) ? geojson : geojson.features,
             i, len, feature;
@@ -129,11 +76,11 @@ L.GeoJSONWithBorder = L.GeoJSON.extend({
 
         if (options.filter && !options.filter(geojson)) { return this; }
 
-        var layer = this.geometryToLayerWithBorder(geojson, options);
+        var layer = L.GeoJSONWithBorder.geometryToLayer(geojson, options);
         if (!layer) {
             return this;
         }
-        layer.feature = L.GeoJSON.asFeature(geojson);
+        layer.feature = L.GeoJSONWithBorder.asFeature(geojson);
 
         layer.defaultOptions = layer.options;
         this.resetStyle(layer);
@@ -145,6 +92,59 @@ L.GeoJSONWithBorder = L.GeoJSON.extend({
         return this.addLayer(layer);
     }
 });
+
+L.GeoJSONWithBorder.geometryToLayer = function(geojson, options) {
+    var geometry = geojson.type === 'Feature' ? geojson.geometry : geojson,
+        coords = geometry ? geometry.coordinates : null,
+        layers = [],
+        pointToLayer = options && options.pointToLayer,
+        _coordsToLatLng = options && options.coordsToLatLng || L.GeoJSONWithBorder.coordsToLatLng,
+        latlng, latlngs, i, len;
+
+    if (!coords && !geometry) {
+        return null;
+    }
+
+    switch (geometry.type) {
+        case 'Point':
+            latlng = _coordsToLatLng(coords);
+            return pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng);
+
+        case 'MultiPoint':
+            for (i = 0, len = coords.length; i < len; i++) {
+                latlng = _coordsToLatLng(coords[i]);
+                layers.push(pointToLayer ? pointToLayer(geojson, latlng) : new L.Marker(latlng));
+            }
+            return new L.FeatureGroup(layers);
+
+        case 'LineString':
+        case 'MultiLineString':
+            latlngs = L.GeoJSONWithBorder.coordsToLatLngs(coords, geometry.type === 'LineString' ? 0 : 1, _coordsToLatLng);
+            return new L.PolylineWithBorder(latlngs, options);
+
+        case 'Polygon':
+        case 'MultiPolygon':
+            latlngs = L.GeoJSONWithBorder.coordsToLatLngs(coords, geometry.type === 'Polygon' ? 1 : 2, _coordsToLatLng);
+            return new L.Polygon(latlngs, options);
+
+        case 'GeometryCollection':
+            for (i = 0, len = geometry.geometries.length; i < len; i++) {
+                var layer = L.GeoJSONWithBorder.geometryToLayer({
+                    geometry: geometry.geometries[i],
+                    type: 'Feature',
+                    properties: geojson.properties
+                }, options);
+
+                if (layer) {
+                    layers.push(layer);
+                }
+            }
+            return new L.FeatureGroup(layers);
+
+        default:
+            throw new Error('Invalid GeoJSON object.');
+    }
+};
 
 L.geoJSONWithBorder = function (geojson, options) {
     return new L.GeoJSONWithBorder(geojson, options);
