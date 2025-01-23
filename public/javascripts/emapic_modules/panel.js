6097cdfddf9e0f91e7eafaf7f499e387f1ee1ca6
Jorge López Fernández <lopez.fernandez.jorge@gmail.com>
2025-01-24 00:42:02 +0100

    Add option for reopening the map's sidebar panel
    Shows a small button on the right side after the panel
    has been closed.

--- public/javascripts/emapic_modules/panel.js
+++ public/javascripts/emapic_modules/panel.js
@@ -12,26 +12,30 @@ var emapic = emapic || {};
             "<div id='indivData'></div>\n" +
             "<div id='navContainer'></div>\n" +
             "<div id='closesidebarPanel'></div>\n" +
+            "<div id='opensidebarPanel' class='hidden'></div>\n" +
             "</div>",
         titlesidebarPanel,
         closesidebarPanel,
+        opensidebarPanel,
         navContainer,
         sidebarPanel,
         closesidebarPanelHtml = "<button type='button' title='" + emapic.utils.getI18n('js_close', 'Cerrar') + "' onclick='emapic.sidebarPanelClose();'><span class='glyphicon glyphicon-chevron-right'></span></button>",
+        opensidebarPanelHtml = "<button type='button' title='" + emapic.utils.getI18n('js_open', 'Abrir') + "' onclick='emapic.modules.panel.sidebarPanelReopen();'><span class='glyphicon glyphicon-chevron-left'></span></button>",
         navHtml = "<button id='FirstButton' title='" + emapic.utils.getI18n('js_go_first', 'Ir al primero') + "' class='btn btn-default btn-xs' onclick='emapic.modules.panel.goToFirst();'><span class='glyphicon glyphicon-fast-backward'></span></button>\n" +
             "<button id='PrevButton' title='" + emapic.utils.getI18n('js_go_previous', 'Ir al anterior') + "' class='btn btn-default btn-xs' onclick='emapic.modules.panel.goToPrevious();'><span class='glyphicon glyphicon-step-backward'></span></button>\n" +
             "<input type='text' id='CurrentFeatureNumber' maxlength='7'></input><label>/</label><label id='TotalFeatures'></label>\n" +
             "<button id='NextButton' title='" + emapic.utils.getI18n('js_go_next', 'Ir al siguiente') + "' class='btn btn-default btn-xs' onclick='emapic.modules.panel.goToNext();'><span class='glyphicon glyphicon-step-forward'></span></button>\n" +
             "<button id='LastButton' title='" + emapic.utils.getI18n('js_go_last', 'Ir al último') + "' class='btn btn-default btn-xs' onclick='emapic.modules.panel.goToLast();'><span class='glyphicon glyphicon-fast-forward'></span></button>\n",
         currentFeature = null,
+        latestFeature = null,
         oldIcon = null,
         currentMarker = null,
         nrLayers,
-        selectedLayerNr,
         layers;
 
     emapic.modules = emapic.modules || {};
     emapic.modules.panel = emapic.modules.panel || {};
+    emapic.modules.panel.allowReopen = false;
 
     emapic.disableIndivLayerExclusiveComponents = emapic.utils.overrideFunction(emapic.disableIndivLayerExclusiveComponents, null, function() {
         if (currentFeature !== null) {
@@ -63,6 +67,7 @@ var emapic = emapic || {};
         emapic.map.addControl(sidebarPanel);
         titlesidebarPanel = $('#titlesidebarPanel');
         closesidebarPanel = $('#closesidebarPanel');
+        opensidebarPanel = $('#opensidebarPanel');
         navContainer = $('#navContainer');
         bodysidebarPanel();
         $('#CurrentFeatureNumber').keydown(featureNumberKeydown);
@@ -71,6 +76,7 @@ var emapic = emapic || {};
     function bodysidebarPanel() {
         navContainer.html(navHtml);
         closesidebarPanel.html(closesidebarPanelHtml);
+        opensidebarPanel.html(opensidebarPanelHtml);
     }
 
     emapic.sidebarPanelClose = emapic.utils.overrideFunction(emapic.sidebarPanelClose, null, function() {
@@ -83,8 +89,8 @@ var emapic = emapic || {};
         return "<h3>" + emapic.utils.getI18n('js_see_answers', 'Ver respuestas') + "</h3>\n";
     };
 
-    emapic.modules.panel.updateData = function(number) {
-        currentFeature = number;
+    emapic.modules.panel.updateData = function(number, silent) {
+        latestFeature = currentFeature = number;
         var currentFeatureInput = $('#CurrentFeatureNumber');
         $('#TotalFeatures').text(nrLayers);
         currentFeatureInput.attr("size", (nrLayers.toString().length < 3) ? 3 : nrLayers.toString().length);
@@ -100,7 +106,9 @@ var emapic = emapic || {};
             titlesidebarPanel.hide();
         }
         $("#indivData").html(emapic.getPopupHtml(layers[number].feature.properties));
-        emapic.showMarker(layers[number]);
+        if (!silent) {
+            emapic.showMarker(layers[number]);
+        }
     };
 
     emapic.modules.panel.getColorPanelSelect = function(number) {
@@ -126,12 +134,15 @@ var emapic = emapic || {};
         });
     };
 
-    emapic.modules.panel.goToRegister = function(number) {
+    emapic.modules.panel.goToRegister = function(number, silent) {
         if (number !== null && currentFeature !== number) {
             emapic.modules.panel.getColorPanelUnselect();
             currentMarker = layers[number];
+            if (emapic.modules.panel.allowReopen) {
+                opensidebarPanel.removeClass('hidden');
+            }
             emapic.modules.panel.getColorPanelSelect(number);
-            emapic.modules.panel.updateData(number);
+            emapic.modules.panel.updateData(number, silent);
         }
     };
 
@@ -159,25 +170,32 @@ var emapic = emapic || {};
         return currentFeature !== null ? layers[currentFeature] : null;
     };
 
+    emapic.modules.panel.sidebarPanelReopen = function() {
+        if (latestFeature) {
+            emapic.modules.panel.goToRegister(latestFeature, true);
+            sidebarPanel.show();
+        }
+    };
+
     emapic.indivVotesLayerOnEachFeature = function(data) {
         data.layer.on('click', function(e) {
             layers = emapic.getIndivVotesLayerLeafletLayers();
             nrLayers = layers.length;
-            selectedLayerNr = null;
+            var newFeature = null;
             for (var i = 0; i < nrLayers; i++) {
                 if (layers[i].feature === data.feature) {
-                    selectedLayerNr = i;
+                    newFeature = i;
                     break;
                 }
             }
-            if (selectedLayerNr === null) {
+            if (newFeature === null) {
                 console.error("Can't find layer for panel.");
                 return;
             }
             if (currentFeature === null) {
                 emapic.sidebarPanelClose();
             }
-            emapic.modules.panel.goToRegister(selectedLayerNr);
+            emapic.modules.panel.goToRegister(newFeature);
             sidebarPanel.show();
         });
         return data;
